475b6349b2702c757d2984ecc11c0cd45fb2847e
nusaibaaaa <nusaibanurauba826@gmail.com>
2026-03-17 23:35:59 +0100

    fixed__name__error

--- app.py
+++ app.py
@@ -1,35 +1,27 @@
-
 from flask import Flask, render_template, request, redirect
-from models import Patient
 from queue_manager import add_patient, next_patient, get_queue, total_seen
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    patients = get_queue()
-    return render_template("index.html", patients=patients)
-
-@app.route("/register", methods=["GET","POST"])
-def register():
-    if request.method == "POST":
-        name = request.form["name"]
-        age = request.form["age"]
-        symptoms = request.form["symptoms"]
-        patient = Patient(name, age, symptoms)
-        add_patient(patient)
-        return redirect("/")
-    return render_template("register.html")
-
-@app.route("/next")
-def call_next():
-    next_patient()
-    return redirect("/")
-
-@app.route("/stats")
-def stats():
+    queue = get_queue()
     total = total_seen()
-    return render_template("stats.html", total=total)
+    return render_template('index.html', queue=queue, total=total)
 
-if __name__ == "__main__":
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form.get('patient')
+
+    if name:
+        add_patient(name)
+
+    return redirect('/')
+
+@app.route('/next')
+def next():
+    next_patient()
+    return redirect('/')
+
+if name == '__main__':
     app.run(debug=True)
