98c4ad5870c58c084ebb803d1dcc494ce9f75917
nusaibaaaa <nusaibanurauba826@gmail.com>
2026-03-17 23:47:46 +0100

    if__call__error

--- app.py
+++ app.py
@@ -3,25 +3,32 @@ from queue_manager import add_patient, next_patient, get_queue, total_seen
 
 app = Flask(__name__)
 
+# Home page
 @app.route('/')
 def home():
-    queue = get_queue()
-    total = total_seen()
+    queue = get_queue()      # get current queue
+    total = total_seen()     # get total served patients
     return render_template('index.html', queue=queue, total=total)
 
+
+# Register patient
 @app.route('/register', methods=['POST'])
 def register():
     name = request.form.get('patient')
 
-    if name:
+    if name:   # simple validation
         add_patient(name)
 
     return redirect('/')
 
+
+# Call next patient
 @app.route('/next')
 def next():
     next_patient()
     return redirect('/')
 
+
+# Run the app
 if name == '__main__':
     app.run(debug=True)
